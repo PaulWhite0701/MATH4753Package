ddafe827db9f8f61cdac3a6a7ee9b952f1970f76
Paul <paul.a.white-1@ou.edu>
2020-01-15 11:05:03 -0600

    Initial commit of files

---
+++ R/myf.R
@@ -0,0 +1,18 @@
+#' My function of squares and cubes
+#'
+#' Produce a list of squares and cubes
+#'
+#' This is a part of the introduction to creating packages. It takes a vector and returns a list of vectors indicating its square and cube values
+#'
+#' @param x a vector
+#'
+#' @return a list of vectors
+#' @export
+#'
+#' @examples
+#' x=1:4;myf(x)
+myf=function(x){
+  obj1=x^2
+  obj2=x^3
+  list(squared=obj1, cubed=obj2)
+}
